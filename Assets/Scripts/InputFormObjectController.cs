216dc36ef631801e8ba4e1a662360c59f74f0a8a
shinnyeonki <sygys10293@gmail.com>
2024-10-14 08:26:31 +0900

    임포트및 main 의 기본 틀 가져오기

---
+++ Assets/Scripts/InputFormObjectController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class InputFormObjectController : MonoBehaviour
+{
+
+    public InputField inputField;
+    public GameObject taskArea;
+    // > .net - Unix time conversions in C# - Stack Overflow  
+    // > https://stackoverflow.com/questions/7983441/unix-time-conversions-in-c-sharp
+    private static readonly DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddTask()
+    {
+        // Todo一覧取得
+        var todoList = StorageManager.GetAll();
+        if (todoList == null) {
+            Debug.Log("todoList : null");
+            todoList = new Dictionary<string, string>();
+        }
+        Debug.Log("todoList count : " + todoList.Count);
+        // Todoに加えて保存
+        var key = Convert.ToString((DateTime.UtcNow - UnixEpoch).TotalMilliseconds);
+        todoList.Add(key, inputField.text);
+        StorageManager.Save(todoList);
+
+        // Todo一覧に加える
+        taskArea.transform.GetComponent<TodoListContentController>().AddTodo(key, inputField.text);
+
+        // フォームをクリアする
+        inputField.text = null;
+    }
+}
---
+++ Assets/Scripts/StorageManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class StorageManager
+{
+
+    private static string masterKey = "e82d33663b9995d23cd93a7a68b782";
+
+    public static Dictionary<string, string> GetAll()
+    {
+        if (!PlayerPrefs.HasKey(masterKey)) {
+            return null;
+        }
+        string value = PlayerPrefs.GetString(masterKey);
+        return Deserialize<Dictionary<string, string>>(value);
+    }
+
+    public static void Save(Dictionary<string, string> todoList) {
+        PlayerPrefs.SetString(masterKey, Serialize<Dictionary<string, string>>(todoList));
+    }
+
+    // > Saving a Dictionary To PlayerPrefs - Unity Answers  
+    // > https://answers.unity.com/questions/1022800/saving-a-dictionary-to-playerprefs.html
+    // > How do I save a custom class of variables to playerprefs? - Unity Answers  
+    // > https://answers.unity.com/questions/610893/how-do-i-save-a-custom-class-of-variables-to-playe.html?_ga=2.36768227.212595546.1570296762-888141676.1564340563
+    private static T Deserialize<T>(string value) where T : class
+    {
+        byte[] bytes = Convert.FromBase64String(value);
+        MemoryStream stream = new MemoryStream(bytes);
+        BinaryFormatter formatter = new BinaryFormatter();
+        return formatter.Deserialize(stream) as T;
+    }
+
+    private static string Serialize<T>(T data) where T : class
+    {
+        // > c# - binary formatter to string - Stack Overflow  
+        // > https://stackoverflow.com/questions/10903327/binary-formatter-to-string
+        var memoryStream = new MemoryStream();
+        var formatter = new BinaryFormatter();
+        using (memoryStream)
+        {
+            formatter.Serialize(memoryStream, data);
+        }
+        return Convert.ToBase64String(memoryStream.ToArray());
+    }
+}
