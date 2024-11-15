eab4beffede2e7c62af589fd050e02a7a14ce3ba
SougHYH <anglee6915@gmail.com>
2024-11-15 16:14:54 +0900

    add monster prefab resource, monster list

--- Assets/Scripts/BaseScene.cs
+++ Assets/Scripts/BaseScene.cs
@@ -1,5 +1,7 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,26 +11,40 @@ public class BaseScene : MonoBehaviour
     public static BaseScene baseScene;
     public GameManager gm;
     public PlayerController player;
+    public List<GameObject> monsterList;
 
     void Start()
     {
         LoadMainScene();
+        InputMonsters();
     }
 
     public void LoadMainScene()
     {
-        if(!gm.IsTodoDoneClear())
+        monsterList = new List<GameObject>();
+        InputMonsters();
+        if (!gm.IsTodoDoneClear())
         {
             player.BattlePosition();
             CreateMonster();
         }
     }
 
-    void CreateMonster()
+    void CreateMonster() //몬스터 생성
     {
-        GameObject monster = Resources.Load("Prefabs/MonsterA") as GameObject;
-        Vector3 pos = new Vector3(1, -1, 0);
+        int random = Random.Range(0, monsterList.Count-1);
+        Vector3 pos = new Vector3(1, -0.75f, 0);
+
+        GameObject monster = monsterList[random];
         monster.transform.position = pos;
-        Instantiate(monster);
+        Instantiate(monsterList[random]);
+    }
+
+    void InputMonsters() //몬스터 리스트 생성
+    {
+        monsterList.Add(Resources.Load("Prefabs/Mushroom") as GameObject);
+        monsterList.Add(Resources.Load("Prefabs/Skeleton") as GameObject);
+        monsterList.Add(Resources.Load("Prefabs/Goblin") as GameObject);
+        monsterList.Add(Resources.Load("Prefabs/Flying eye") as GameObject);
     }
 }
